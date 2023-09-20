73668de0dcf4ea4ca384919109f66feeb244c3da
igorir3 <97892348+igorir3@users.noreply.github.com>
2023-09-20 15:07:21 +0300

    0.1 update
    Добавлена простейшая система сравнивания слов со внутренним списком, а также система конфигов

---
+++ main.py
@@ -0,0 +1,40 @@
+import telebot
+import time
+import datetime
+import configparser
+import os
+
+if os.path.exists('config.ini'):
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    token = config.get('MAIN', "token")
+else:
+    config = configparser.ConfigParser()
+    config.add_section("MAIN")
+    token='6643667497:AAHhc_zwDbII4_5ETIp7xsLLUS2KBIq1Y40'
+    config.set("MAIN", "token", token)
+    with open("config.ini", "w") as config_file:
+        config.write(config_file)
+    config_file.close()
+    
+
+bot=telebot.TeleBot(token)
+
+@bot.message_handler(content_types=['text'])
+def main_thread(message):
+    TeMessage = message.text
+    TMList = TeMessage.split()
+
+    ScoreWords = ["привет", "пока", "что"]
+    ScoresofW = [1, 2, 3]
+    Score = 0
+    
+    TMList = list(map(lambda x: x.lower(), TMList))
+    
+    for o in range(len(TMList)):
+        if TMList[o] in ScoreWords:
+            Score = Score + ScoresofW[ScoreWords.index(TMList[o])]
+
+    print(f"{message.text} - {Score}")
+
+bot.infinity_polling()
